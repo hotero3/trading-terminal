0315917c78d589d1405cb2c311d6c755ba1d2db7
hotero3 <151691490+hotero3@users.noreply.github.com>
2026-01-11 07:52:20 -0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,10 +1,10 @@
 // 1. Cargar URLs guardadas en el iPad
 window.addEventListener('load', () => {
-    const u2 = localStorage.getItem('u2');
-    const u3 = localStorage.getItem('u3');
+    const u1 = localStorage.getItem('u1_ipad');
+    const u2 = localStorage.getItem('u2_ipad');
     
+    if (u1) document.getElementById('v1').src = u1;
     if (u2) document.getElementById('v2').src = u2;
-    if (u3) document.getElementById('v3').src = u3;
 });
 
 // 2. Botones de URL
@@ -12,11 +12,11 @@ document.querySelectorAll('.btn-url').forEach(boton => {
   boton.addEventListener('click', () => {
     const idIframe = boton.getAttribute('data-id');
     const iframe = document.getElementById(idIframe);
-    const newUrl = prompt("URL para este panel:", iframe.src);
+    const newUrl = prompt("Ingresa la URL para este panel:", iframe.src);
     
     if (newUrl && newUrl !== "about:blank") {
       iframe.src = newUrl;
-      localStorage.setItem('u' + idIframe.charAt(1), newUrl);
+      localStorage.setItem('u' + idIframe.charAt(1) + '_ipad', newUrl);
     }
   });
 });
@@ -36,5 +36,4 @@ document.querySelectorAll('.btn-zoom').forEach(boton => {
 document.getElementById('btnCloseZoom').addEventListener('click', () => {
   document.getElementById('zoomBox').style.display = 'none';
   document.getElementById('zoomFrame').src = '';
-
 });
